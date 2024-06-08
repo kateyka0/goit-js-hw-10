2b45e2d11972ccba718afae8ead7ea3b4ccc070e
Katrin <kateyka1410@gmail.com>
2024-06-08 18:06:40 +0300

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,24 +1,20 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const form = document.querySelector('.form');
-
-form.addEventListener('submit', (e) => {
+document.querySelector('.form').addEventListener('submit', function (e) {
   e.preventDefault();
 
-  const delay = parseInt(form.elements.delay.value);
-  const state = form.elements.state.value;
+  const delay = parseInt(document.querySelector('input[name="delay"]').value);
+  const state = document.querySelector('input[name="state"]:checked').value;
 
   createPromise(delay, state)
     .then((delay) => {
       iziToast.success({
-        title: 'Success',
         message: `✅ Fulfilled promise in ${delay}ms`,
       });
     })
     .catch((delay) => {
       iziToast.error({
-        title: 'Error',
         message: `❌ Rejected promise in ${delay}ms`,
       });
     });
